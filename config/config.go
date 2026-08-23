17132cd122be1d002e932cce41eea9aadeb57c5e
Pidd-pipi <Pidd-pipi@users.noreply.github.com>
2026-08-23 17:31:50 +0800

    feat: initial import

---
+++ config/config.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+func Port() int {
+	p, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil || p < 1 || p > 65535 {
+		return 8080
+	}
+	return p
+}
